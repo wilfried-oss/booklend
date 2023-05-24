3c45205eade9691e402d89ca002dd44f2a196230
wilfried-oss <davowilfried3@gmail.com>
2023-05-24 17:19:50 +0100

    layout/theme.blade  modification

--- resources/views/layout/theme.blade.php
+++ resources/views/layout/theme.blade.php
@@ -26,9 +26,9 @@
         <!-- partial:partials/_navbar.html -->
         <nav class="navbar default-layout-navbar col-lg-12 col-12 p-0 fixed-top d-flex flex-row">
             <div class="text-center navbar-brand-wrapper d-flex align-items-center justify-content-center">
-                <a class="navbar-brand brand-logo" href="index.html"><img src="{{ asset('assets/images/logo.svg') }} "
+                <a class="navbar-brand brand-logo" href="#"><img src="{{ asset('assets/images/logo.svg') }} "
                         alt="logo" /></a>
-                <a class="navbar-brand brand-logo-mini" href="index.html"><img
+                <a class="navbar-brand brand-logo-mini" href="#"><img
                         src="{{ asset('assets/images/logo-mini.svg') }}" alt="logo" /></a>
             </div>
             <div class="navbar-menu-wrapper d-flex align-items-stretch">
